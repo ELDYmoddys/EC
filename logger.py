9ea7b1d10f5408ce07d34426e087439460193481
eldym <eldymoddys@gmail.com>
2024-03-30 06:21:29 -0400

    Removed direct references to file locations on my own computer (oops)
    Added reading and writing to log functions
    generate_hash - generates some random hash to create unique transaction "IDs" using the user's id and txn time
    fetch_txns - fetches a list of transactions by a user

--- logger.py
+++ logger.py
@@ -3,38 +3,58 @@ from datetime import datetime
 from deltalake.writer import write_deltalake
 
 class Logger:
-    def create_txn_log(giver_uid, reciever_uid, amount):
+    def create_txn_log(giver_uid: int, reciever_uid: list, amount: list):
+
         txn_hash = generate_hash(giver_uid)
         timestamp = get_time()
         
         txn_data_list = [(txn_hash, timestamp, giver_uid, reciever_uid, amount)]
         txn_dataframe_to_app = pd.DataFrame(txn_data_list, columns=['txn_hash', 'timestamp', 'giver_uid', 'reciever_uid', 'amount'])
 
-        write_deltalake(r'C:\Users\eldym\OneDrive\Documents\EC_pandas\log.parquet', txn_dataframe_to_app, mode="append")
+        write_logs(txn_dataframe_to_app)
 
         return txn_hash
 
-    def read_txn_log(txn_hash):
+    def read_txn_log(txn_hash: str):
 
-        df = pd.read_parquet(r'C:\Users\eldym\OneDrive\Documents\EC_pandas\log.parquet')
+        df = read_logs()
     
         txn_data = df.loc[df['txn_hash'] == txn_hash]
 
         if not txn_data.empty:
-            return [txn_data['timestamp'], txn_data['giver_uid'], txn_data['reciever_uid'], txn_data['amount']]
+            return txn_data
         else:
             return False
         
-    def fetch_txns(uid, page):
+    def fetch_txns(uid: int, page: int):
 
-        
+        df = read_logs()
+
+        df = df.loc[df[f'giver_uid'] == uid][page*10-10:page*10]
+
+        return df['txn_hash'].tolist()
 
 
+def generate_hash(uid: int):
 
-def generate_hash(uid):
+    tup = uid, get_time()
 
-    return hex(abs(hash(uid, get_time())))
+    return hex(abs(hash(tup)))
 
 def get_time():
 
-    return int(datetime.timestamp(datetime.now))
+    time = int(datetime.timestamp(datetime.now()))
+
+    return time
+
+def read_logs():
+
+    df = pd.read_parquet(r'txn_log.parquet')
+
+    return df
+
+def write_logs(df):
+
+    write_deltalake(r'txn_log.parquet', df, mode="append")
+
+    return
