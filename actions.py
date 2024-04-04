09778c5ac47f61ca2d045fc7a81026d0eed81e23
eldym <eldymoddys@gmail.com>
2024-04-04 13:31:37 -0400

    transaction should transact now added the generated txn_log and user_info parquet files

--- actions.py
+++ actions.py
@@ -30,10 +30,17 @@ class Actions:
         reciever_info = Actions.fetch_balance(reciever)
 
         if giver_info is not False and reciever_info is not False:
-            if float(f'0.' + '0'*(DECIMALS-1) + '1'): # checks if the amount sent is within 
+            if float(f'0.' + '0'*(DECIMALS-1) + '1'): # checks if the amount is within the minimum threshold
                 if giver_info[0] >= amount:
-                    txn_hash = Logger.create_txn_log()
-                    # WIP - need to modify user data here!                
+                    try: 
+                        df = read_bals()
+                        df.loc[df['uid'] == giver]['balance'] -= amount
+                        df.loc[df['uid'] == reciever]['balance'] += amount
+                        write_bals(df)
+                    except:
+                        fail_case = 'error_modify'
+                    else:
+                        txn_hash = Logger.create_txn_log()               
                 else:
                     fail_case = 'bal_too_low'
             else:
@@ -53,3 +60,9 @@ def read_bals(): # gets balance data
     df = pd.read_parquet(r'user_info.parquet')
 
     return df
+
+def write_bals(df): # takes dataframe and overwrites the balance info
+
+    write_deltalake(r'user_info.parquet', df, mode="overwrite")
+
+    return
