f5d4f0c638ab3a8023583abd7ab44935a2e62654
eldym <eldymoddys@gmail.com>
2024-04-14 15:03:12 -0400

    config file for customiation and other data

---
+++ config.py
@@ -0,0 +1,6 @@
+PREFIXES = ['!', 'ec!'] # add/change/remove accordingly to your usage
+CURRENCY = 'EC' # change to your currency ticker
+'COOLDOWN = 2 # cooldown between messages (seconds)'
+DECIMALS = 6 # decimal places for your currency (ie. with 6, the smallest division is 0.000001)
+EMB_COLOUR = 0x000000 # colour of embed (hex)
+TOKEN = 'Your Bot Token Here!' # put your bot token here
