81e5be62c24c71c01664af2c592642704ee0ff32
eldym <eldymoddys@gmail.com>
2024-04-14 15:05:01 -0400

    refer to config file for custom variables

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import discord
 import asyncio
 
+from discord import app_commands
 from discord.ext import tasks, commands
+from config import PREFIXES, CURRENCY, DECIMALS, EMB_COLOUR, TOKEN
 from datetime import datetime
 from actions import Actions
-
-PREFIXES = ['!', 'ec!'] # add/change/remove accordingly to your usage
-CURRENCY = 'EC' # change to your currency ticker
-COOLDOWN = 2 # cooldown between messages (seconds)
-DECIMALS = 6 # decimal places for your currency (ie. with 6, the smallest division is 0.000001)
-EMB_COLOUR = 0x000000 # colour of embed (hex)
 
 client = commands.Bot(command_prefix = PREFIXES, intents=discord.Intents.all(), help_command=None)
 
@@ -107,8 +103,5 @@ async def pay(ctx, reciever, amount):
         await ctx.send(f"<@{ctx.author.id}>", embed=embed)
 
 
-def build_embed(emb_type, *extra_info):
-    # not useful yet
-    pass
 
-client.run('your discord bot token')
+client.run(TOKEN)
